32c8dcd90766ea12774b3d2ea27824d021d97e10
MUTINTA <JustinMutinta@hotmail.com>
2021-08-26 11:55:10 -0500

    Love Calculator

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-year = int(input("What year would you like to check? "))
+"""
+The Love Calculator will calculate the probability of love between you and another person.
+You will enter your name and their name.
+After adding the number of times the letters in 'true love' show up will decide how compatible you are.
+"""
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("This is a leap year")
-        else:
-            print("This is not a leap year")
-    else:
-        print("This is a leap year")
+print("Welcome to the Love Calculator")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+#.lower()
+#.count("a")
+
+combined_string = name1 + name2
+lower_case_string = combined_string.lower()
+
+t = lower_case_string.count("t")
+r = lower_case_string.count("r")
+u = lower_case_string.count("u")
+e = lower_case_string.count("e")
+
+l = lower_case_string.count("l")
+o = lower_case_string.count("o")
+v = lower_case_string.count("v")
+e = lower_case_string.count("e")
+
+score = str(t + r + u + e) + str(l + o + v + e)
+int_score = int(score)
+
+#print(score)
+
+if (int_score < 10) or (int_score > 90):
+    print(f"Your score is {int_score}, you go together like coke and mentos")
+elif (int_score >= 40) or (int_score <= 50):
+    print(f"Your score is {int_score}, you are alright together")
 else:
-    print("Year is not a leap year")
+    print(f"Your score is {int_score}")
